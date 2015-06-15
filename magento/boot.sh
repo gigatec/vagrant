8a26973c99add83b4bdcb61c022f2fc16b549375
Stefan Kreuter <kreuter@gigatec.de>
2015-06-15 22:15:41 +0200

    bug fix + magento urls

--- magento/boot.sh
+++ magento/boot.sh
@@ -6,3 +6,17 @@ cp local.xml.vagrant local.xml
 
 # Import DB (if not exists)
 mageimport
+
+# Output Magento links
+# Get Magento settings
+cd /vagrant/public/
+if [ $(id -u) = "0" ]; then
+	MAGE_PREFIX="$(su vagrant -c "magerun db:info prefix" 2> /dev/null)"
+	MAGE_BACKEND="$(echo "Mage::getConfig()->getNode('admin/routers/adminhtml/args/frontName')->asArray()" | su vagrant -c "magerun -q -n dev:console" 2> /dev/null | sed -n 's/^=> "\([^"]*\)"/\1/gp')"
+else
+	MAGE_PREFIX="$(magerun db:info prefix 2> /dev/null)"
+	MAGE_BACKEND="$(echo "Mage::getConfig()->getNode('admin/routers/adminhtml/args/frontName')->asArray()" | magerun -q -n dev:console 2> /dev/null | sed -n 's/^=> "\([^"]*\)"/\1/gp')"
+fi
+
+echo 'Magento Frontend: http://localhost:8080/'
+echo 'Magento Backend: http://localhost:8080/'$MAGE_BACKEND'/'
