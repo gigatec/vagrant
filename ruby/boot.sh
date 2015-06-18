6f2a537fcf61b796bade576a0291096e85c502e0
Stefan Kreuter <kreuter@gigatec.de>
2015-06-18 19:10:30 +0200

    ruby: /rails/ -> /public/

--- ruby/boot.sh
+++ ruby/boot.sh
@@ -6,13 +6,13 @@ export RAILS_ENV=${RAILS_ENV:-development}
 echo "Loading Rails environment: $RAILS_ENV"
 
 # Remount Dynamic data
-mkdir -m 777 -p /vagrant_guest/rails/tmp /vagrant_guest/rails/log
-mount -o bind /vagrant_guest/rails/tmp /vagrant/rails/tmp
-mount -o bind /vagrant_guest/rails/log /vagrant/rails/log
+mkdir -m 777 -p /vagrant_guest/public/tmp /vagrant_guest/public/log
+mount -o bind /vagrant_guest/public/tmp /vagrant/public/tmp
+mount -o bind /vagrant_guest/public/log /vagrant/public/log
 
 # Import DB (if not exists)
 rubyimport
 
 # Run Rails server
-rm -f /vagrant/rails/tmp/pids/server.pid
-su - vagrant -c "cd /vagrant/rails; export RAILS_ENV=$RAILS_ENV; bundle exec rails s -d"
+rm -f /vagrant/public/tmp/pids/server.pid
+su - vagrant -c "cd /vagrant/public; export RAILS_ENV=$RAILS_ENV; bundle exec rails s -d"
--- ruby/init.sh
+++ ruby/init.sh
@@ -12,11 +12,11 @@ sudo chmod +x /usr/local/bin/*
 # Custom Linux/Bash settings
 echo 'alias dir="ls -al"' >> /etc/profile
 echo 'alias root="sudo -i"' >> /etc/profile
-echo 'cd /vagrant/rails' >> /etc/profile
+echo 'cd /vagrant/public' >> /etc/profile
 
 # Install imagemagick
 echo 'Install imagemagick...'
 apt-get install -y imagemagick libmagickwand-dev
 
 # Install bundler & init project
-su - vagrant -c 'cd /vagrant/rails/; gem install bundler; rbenv rehash; bundle install'
+su - vagrant -c 'cd /vagrant/public/; gem install bundler; rbenv rehash; bundle install'
