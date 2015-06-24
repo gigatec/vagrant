9f6cc81187af318051886d5a15fa621a6f6c932e
Stefan Kreuter <kreuter@gigatec.de>
2015-06-24 16:39:16 +0200

    add wordpress files

---
+++ wordpress/boot.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Import DB (if not exists)
+wpimport
+
+echo 'Frontend: http://localhost:8080/'
+echo 'PHPMyAdmin: http://localhost:8080/phpmyadmin/ (User: root, Pass: )'
+echo 'Webmail: http://localhost:8080/roundcube/ (User: vagrant, Pass: vagrant, Host: localhost)'
