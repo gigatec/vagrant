45437e83ee0c0d2ba444f5b2ef1172bca8343069
Stefan Kreuter <kreuter@gigatec.de>
2016-10-26 09:20:44 +0200

    restructure docker directory (14.04)

--- docker/run.sh
+++ docker/run.sh
@@ -9,6 +9,57 @@
 /etc/init.d/ondemand start
 /etc/init.d/rc.local start
 
-/bin/sh /vagrant/vagrant/wordpress/boot.sh
+case "$DOCKER_SYSTEM" in
+
+wordpress)
+	# Import DB (if not exists)
+	su - vagrant -c "wpimport"
+
+	echo 'Frontend: http://localhost:8080/'
+	echo 'PHPMyAdmin: http://localhost:8080/phpmyadmin/ (User: root, Pass: )'
+	echo 'Webmail: http://localhost:8080/roundcube/ (User: vagrant, Pass: vagrant, Host: localhost)'
+	;;
+
+magento)
+	# Create local.xml Symlink
+	cd /vagrant/public/$1/app/etc/
+	cp local.xml.vagrant local.xml
+
+	# Import DB (if not exists)
+	mageimport $1
+
+	# Output Magento links
+	# Get Magento settings
+	cd /vagrant/public/$1
+	if [ $(id -u) = "0" ]; then
+		MAGE_PREFIX="$(su vagrant -c "magerun db:info prefix" 2> /dev/null)"
+		MAGE_BACKEND="$(echo "Mage::getConfig()->getNode('admin/routers/adminhtml/args/frontName')->asArray()" | su vagrant -c "magerun -q -n dev:console" 2> /dev/null | sed -n 's/^=> "\([^"]*\)"/\1/gp')"
+	else
+		MAGE_PREFIX="$(magerun db:info prefix 2> /dev/null)"
+		MAGE_BACKEND="$(echo "Mage::getConfig()->getNode('admin/routers/adminhtml/args/frontName')->asArray()" | magerun -q -n dev:console 2> /dev/null | sed -n 's/^=> "\([^"]*\)"/\1/gp')"
+	fi
+
+	# run custom boot.sh if available
+	if [ -f "/vagrant/vagrant.boot.sh" ]; then
+		dos2unix /vagrant/vagrant.boot.sh
+		sh /vagrant/vagrant.boot.sh
+	fi
+
+	echo 'Magento Frontend: http://localhost:8080/'
+	echo 'Magento Backend: http://localhost:8080/'$MAGE_BACKEND'/'
+	echo 'PHPMyAdmin: http://localhost:8080/phpmyadmin/ (User: root, Pass: )'
+	echo 'Webmail: http://localhost:8080/roundcube/ (User: vagrant, Pass: vagrant, Host: localhost)'
+	;;
+
+*)
+	# Import DB (if not exists)
+	dbimport
+
+	echo 'Frontend: http://localhost:8080/'
+	echo 'PHPMyAdmin: http://localhost:8080/phpmyadmin/ (User: root, Pass: )'
+	echo 'Webmail: http://localhost:8080/roundcube/ (User: vagrant, Pass: vagrant, Host: localhost)'
+	;;
+
+esac
 
 sleep infinity
